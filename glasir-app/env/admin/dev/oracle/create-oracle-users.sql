1277a65d2555f63e10b015f3b47b46e8413ee8c6
Marcus Pemer <marcus@iteego.com>
2011-12-02 20:10:05 -0500

    * Created the concept of "flavor" to an environment module, where a specific environment module can be instantiated with a specific flavor. Flavors can be used to invoke for example a specific database, as an override to the default H2 database. When invoking "gradle startDevStore", the default flavor of h2 is invoked by using the module "env.store.dev". When invoking "gradle startDevStore -Pflavor=oracle", the oracle flavor is invoked by using the module "env.store.dev.oracle". Supported flavors are currently "oracle", "hsql-mem" and "hsql-file". * Renamed all data sources, removing any mention of ATG and replacing it with Glasir, replacing any mention of Catalog and replacing it with Switching

---
+++ glasir-app/env/admin/dev/oracle/create-oracle-users.sql
@@ -0,0 +1,46 @@
+
+drop user glasir_core cascade;
+create user glasir_core identified by password;
+grant all privileges to glasir_core;
+
+-- To avoid XAException.XAER_RMERR
+grant select on sys.dba_pending_transactions to glasir_core;
+grant select on sys.pending_trans$ to glasir_core;
+grant select on sys.dba_2pc_pending to glasir_core;
+grant execute on sys.dbms_system to glasir_core;
+
+drop user glasir_cat_a cascade;
+create user glasir_cat_a identified by password;
+grant all privileges to glasir_cat_a;
+
+-- To avoid XAException.XAER_RMERR
+grant select on sys.dba_pending_transactions to glasir_cat_a;
+grant select on sys.pending_trans$ to glasir_cat_a;
+grant select on sys.dba_2pc_pending to glasir_cat_a;
+grant execute on sys.dbms_system to glasir_cat_a;
+
+drop user glasir_cat_b cascade;
+create user glasir_cat_b identified by password;
+grant all privileges to glasir_cat_b;
+
+-- To avoid XAException.XAER_RMERR
+grant select on sys.dba_pending_transactions to glasir_cat_b;
+grant select on sys.pending_trans$ to glasir_cat_b;
+grant select on sys.dba_2pc_pending to glasir_cat_b;
+grant execute on sys.dbms_system to glasir_cat_b;
+
+drop user glasir_pub cascade;
+create user glasir_pub identified by password;
+grant all privileges to glasir_pub;
+
+-- To avoid XAException.XAER_RMERR
+grant select on sys.dba_pending_transactions to glasir_pub;
+grant select on sys.pending_trans$ to glasir_pub;
+grant select on sys.dba_2pc_pending to glasir_pub;
+grant execute on sys.dbms_system to glasir_pub;
+
+commit;
+
+-- for the grants, see the following url:
+-- http://docs.codehaus.org/display/BTM/FAQ#FAQ-WhyisOraclethrowingaXAExceptionduringinitializationofmydatasource%3F
+
