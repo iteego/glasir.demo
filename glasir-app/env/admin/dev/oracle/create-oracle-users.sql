31f086bc43e4bae6d1ee126c474b34c951af2a23
Marcus Pemer <marcus@iteego.com>
2011-12-04 14:41:00 -0500

    Refactored location of database and data import files Removed fake version file store Refactored manifest files for admin node, so that full user interface now shows up

--- glasir-app/env/admin/dev/oracle/create-oracle-users.sql
+++ glasir-app/env/admin/dev/oracle/create-oracle-users.sql
@@ -9,25 +9,25 @@ grant select on sys.pending_trans$ to glasir_core;
 grant select on sys.dba_2pc_pending to glasir_core;
 grant execute on sys.dbms_system to glasir_core;
 
-drop user glasir_cat_a cascade;
-create user glasir_cat_a identified by password;
-grant all privileges to glasir_cat_a;
+drop user glasir_a cascade;
+create user glasir_a identified by password;
+grant all privileges to glasir_a;
 
 -- To avoid XAException.XAER_RMERR
-grant select on sys.dba_pending_transactions to glasir_cat_a;
-grant select on sys.pending_trans$ to glasir_cat_a;
-grant select on sys.dba_2pc_pending to glasir_cat_a;
-grant execute on sys.dbms_system to glasir_cat_a;
+grant select on sys.dba_pending_transactions to glasir_a;
+grant select on sys.pending_trans$ to glasir_a;
+grant select on sys.dba_2pc_pending to glasir_a;
+grant execute on sys.dbms_system to glasir_a;
 
-drop user glasir_cat_b cascade;
-create user glasir_cat_b identified by password;
-grant all privileges to glasir_cat_b;
+drop user glasir_b cascade;
+create user glasir_b identified by password;
+grant all privileges to glasir_b;
 
 -- To avoid XAException.XAER_RMERR
-grant select on sys.dba_pending_transactions to glasir_cat_b;
-grant select on sys.pending_trans$ to glasir_cat_b;
-grant select on sys.dba_2pc_pending to glasir_cat_b;
-grant execute on sys.dbms_system to glasir_cat_b;
+grant select on sys.dba_pending_transactions to glasir_b;
+grant select on sys.pending_trans$ to glasir_b;
+grant select on sys.dba_2pc_pending to glasir_b;
+grant execute on sys.dbms_system to glasir_b;
 
 drop user glasir_pub cascade;
 create user glasir_pub identified by password;
