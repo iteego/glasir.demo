2b3417e324e9a2b73bf78cc69f6b7331c08eeece
Matias Bjarland <matias@iteego.com>
2012-04-05 23:20:11 +0200

    added another sample geb spec, fixed windows builds - they should now work, removed console coloring from windows builds by changing the gradlew.bat file, removed debugging properties from the gradle.properties file as they caused multiple concurrent gradle builds to hit 'port already in use' issues

---
+++ glasir-app/Main/Storefront/src/test/groovy/com/glasir/geb/specs/CartGebSpec.groovy
@@ -0,0 +1,27 @@
+package com.glasir.geb.specs
+
+import spock.lang.*
+import geb.*
+import geb.spock.*
+import com.glasir.geb.pages.LoginPage
+import com.glasir.geb.pages.HomePage
+
+
+//for a description of BDD, see http://en.wikipedia.org/wiki/Behavior_Driven_Development
+class CartGebSpec extends GebReportingSpec {
+
+  def "should display item in cart when user hits the add-to-cart button"() {
+    when: "we are on a product details page"
+    and:  "we select a specific product size and color"
+    and:  "we hit the add-to-cart button"
+    
+    then: "the correct product should be displayed in the cart widget"
+  }  
+  
+  @Ignore
+  def "should do something we haven't implemented yet"() {
+  	when: "using a page that doesn't exist yet"
+  	
+  	then: "some condition should be met"
+  }
+}
